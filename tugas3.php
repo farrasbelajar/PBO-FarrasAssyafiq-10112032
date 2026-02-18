9daa9802505bca14873eb75744f2dd1147dfd160
farrasbelajar <farrasbelajar1@gmail.com>
2026-02-18 18:39:50 +0700

    Update tugas3.php

--- tugas3.php
+++ tugas3.php
@@ -1,53 +1,36 @@
 <html>
     <head>
-    <title>
-        toko pegadaian syariah
-    </title>
-
-
-
-
+        <title>
+            toko pegadaian syariah
+        </title>
     </head>
 
     <body>
+
         <h2>
             TOKO PEGADAIAN SYARIAH
         </h2>
         <h3>
             jalan keadilan no 16
         </h3>
-         <h3>
-            telpon : 08222221380
+        <h3>
+            telpon : 72353459
         </h3>
-         <h3>
+        <h3>
             program menghitung angsuran hutang
         </h3>
-        <form action="proses_produk.php" method="POST">
+
+        <form action="tugas3.1.php" method="POST">
             Besaran pinjaman:
             <input type="number" name="pinjaman" value=""> 
             Lama Angsuran (bulan):
             <input type="number" name="lama_angsuran" value="">  
-            Besaran Angsuran:
+            Keterlambatan (hari):
             <input type="number" name="terlambat" value=""> 
 
             
             <input type="submit" value="kirim">
-
-
-
         </form>
 
     </body>
-
-
-
-
 </html>
-
-
-
-
-
-
-
-
