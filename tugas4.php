a01a8537aa001ab9701c17f5495f419c04573671
farrasbelajar <farrasbelajar1@gmail.com>
2026-02-28 14:15:11 +0700

    fix

--- tugas4.php
+++ tugas4.php
@@ -1,56 +1,49 @@
 <html>
-    <head>
-        <title>
-            MENGHITUNG NILAI LULUS
-        </title>
-    </head>
+<head>
+    <title>MENGHITUNG NILAI LULUS</title>
+</head>
 
-    <body>
+<body>
 
-        <h2>
-            MENGHITUNG NILAI LULUS
-        </h2>
-        
-        Mahasiswa 1
-        <form action="tugas4.1.php" method="POST">
-            Nama Mahasiswa:
-            <input type="text" name="nama_mahasiswa" value=""> 
-            Kelas Mahasiswa:
-            <input type="text" name="kelas_mahasiswa" value="">  
-            Mata Kuliah
-            <input type="text" name="mata_kuliah" value=""> 
-            Nilai Mahasiswa:
-            <input type="number" name="nilai_mahasiswa" value=""> <br>
-        <br>
-        Mahasiswa 2 <br>
-        
-            Nama Mahasiswa:
-            <input type="text" name="nama_mahasiswa" value=""> 
-            Kelas Mahasiswa:
-            <input type="text" name="kelas_mahasiswa" value="">  
-            Mata Kuliah
-            <input type="text" name="mata_kuliah" value=""> 
-            Nilai Mahasiswa:
-            <input type="number" name="nilai_mahasiswa" value=""> <br>
+<h2>MENGHITUNG NILAI LULUS</h2>
 
-            <br>
-            Mahasiswa 3 <br>
-            Nama Mahasiswa:
-            <input type="text" name="nama_mahasiswa" value=""> 
-            Kelas Mahasiswa:
-            <input type="text" name="kelas_mahasiswa" value="">  
-            Mata Kuliah
-            <input type="text" name="mata_kuliah" value=""> 
-            Nilai Mahasiswa:
-            <input type="number" name="nilai_mahasiswa" value=""> <br>
-            
-            
+<form action="tugas4.1.php" method="POST">
+
+    <h3>Mahasiswa 1</h3>
+    Nama Mahasiswa:
+    <input type="text" name="nama1"><br>
+    Kelas Mahasiswa:
+    <input type="text" name="kelas1"><br>
+    Mata Kuliah:
+    <input type="text" name="mapel1"><br>
+    Nilai Mahasiswa:
+    <input type="number" name="nilai1"><br><br>
 
 
+    <h3>Mahasiswa 2</h3>
+    Nama Mahasiswa:
+    <input type="text" name="nama2"><br>
+    Kelas Mahasiswa:
+    <input type="text" name="kelas2"><br>
+    Mata Kuliah:
+    <input type="text" name="mapel2"><br>
+    Nilai Mahasiswa:
+    <input type="number" name="nilai2"><br><br>
 
 
-            <input type="submit" value="kirim">
-        </form>
+    <h3>Mahasiswa 3</h3>
+    Nama Mahasiswa:
+    <input type="text" name="nama3"><br>
+    Kelas Mahasiswa:
+    <input type="text" name="kelas3"><br>
+    Mata Kuliah:
+    <input type="text" name="mapel3"><br>
+    Nilai Mahasiswa:
+    <input type="number" name="nilai3"><br><br>
 
-    </body>
+    <input type="submit" value="Kirim">
+
+</form>
+
+</body>
 </html>
